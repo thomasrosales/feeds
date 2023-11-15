97d8216129d3ce996a2ad424d0369fed671daddf
Tomás Rosales <tomasjoaquinrosales@gmail.com>
2023-11-15 16:28:22 -0300

    bugfix: import sginals to user app removed

--- feeds_for_sendcloud/users/apps.py
+++ feeds_for_sendcloud/users/apps.py
@@ -5,9 +5,3 @@ from django.utils.translation import gettext_lazy as _
 class UsersConfig(AppConfig):
     name = "feeds_for_sendcloud.users"
     verbose_name = _("Users")
-
-    def ready(self):
-        try:
-            import feeds_for_sendcloud.users.signals  # noqa: F401
-        except ImportError:
-            pass
