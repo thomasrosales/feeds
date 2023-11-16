31c2a2c18730137678e3feaaff7c6683a312be53
Tomás Rosales <tomasjoaquinrosales@gmail.com>
2023-11-16 09:44:11 -0300

    readme updated to explain local deployment. Also, was added to poetry envlet package in case windows users want to run celery

--- feeds_for_sendcloud/utils/rss.py
+++ feeds_for_sendcloud/utils/rss.py
@@ -2,6 +2,13 @@ import feedparser
 
 
 def parse_rss_to_dict(rss_content: str) -> dict:
+    """
+    If the rss_content is an invalid rss xml format
+    the result will contain the same structure as
+    if it was a valid format, therefore when the backend
+    models process the "entries" key it wil contain an empty
+    list.
+    """
     return feedparser.parse(rss_content)
 
 
