edeee4c7664d668c3a936d8bfc31fc603b298c89
Tomás Rosales <tomasjoaquinrosales@gmail.com>
2023-11-18 20:52:15 -0300

    gunicorn added as a server.

--- docker/scripts/start.sh
+++ docker/scripts/start.sh
@@ -7,4 +7,4 @@ set -o nounset
 poetry run python manage.py migrate --no-input
 poetry run python manage.py collectstatic --noinput
 poetry run python manage.py create_user_feeds_for_testing_purpose
-poetry run python manage.py runserver 0.0.0.0:8000 --noreload --nothreading
+poetry run gunicorn config.wsgi --bind 0.0.0.0:8000
