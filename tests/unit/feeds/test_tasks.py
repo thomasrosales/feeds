1e1581811ac87e293d223231e7c31ad4542a1ebb
Tomás Rosales <tomasjoaquinrosales@gmail.com>
2023-11-18 16:15:10 -0300

    new test added to verify tasks mechanims and also filter improvement

--- tests/unit/feeds/test_tasks.py
+++ tests/unit/feeds/test_tasks.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import datetime, timedelta, timezone
 from unittest.mock import patch
 
@@ -18,13 +19,14 @@ def test_process_posts(mock_process_source_posts, feed):
 
 
 @patch("feeds_for_sendcloud.feeds.tasks.update_single_feed_posts")
-def test_process_feeds_posts(mock_update_single_feed_posts):
+def test_process_feeds_posts(mock_update_single_feed_posts, caplog):
     date_last_refresh = datetime.now(timezone.utc) - timedelta(minutes=6)
     feeds = FeedFactory.create_batch(size=5, state="updated", last_refresh=date_last_refresh)
 
     process_feeds_posts()
 
     assert mock_update_single_feed_posts.delay.call_count == len(feeds)
+    assert caplog.messages[0] == "Feeds to be processing: 5"
 
 
 @patch("feeds_for_sendcloud.feeds.tasks.update_single_feed_posts")
@@ -37,6 +39,16 @@ def test_process_feeds_posts_up_to_date(mock_update_single_feed_posts):
     mock_update_single_feed_posts.delay.assert_not_called()
 
 
+def test_update_single_feed_posts_success(feed, caplog):
+    feed.state = "updated"
+    feed.save()
+
+    with patch.object(Feed, "process_source_posts"):
+        update_single_feed_posts(feed.pk)
+
+    assert caplog.messages[0] == f"Feed({feed.pk}) updated"
+
+
 @patch("feeds_for_sendcloud.feeds.tasks.update_single_feed_posts.retry")
 def test_update_single_feed_posts_retry(mock_update_single_feed_posts_retry, feed):
     feed.state = "invalid"
@@ -46,3 +58,8 @@ def test_update_single_feed_posts_retry(mock_update_single_feed_posts_retry, fee
 
     with pytest.raises(Retry):
         update_single_feed_posts(feed.pk)
+
+
+def test_update_single_feed_posts_object_does_not_exists(caplog):
+    update_single_feed_posts(999)
+    assert caplog.messages[0] == "Feed(999) was removed"
